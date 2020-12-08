57c9bc7870fa0d830f081f0ead9d59f3a4cd7e08
jswpark <jswpark@gmail.com>
2020-12-08 15:10:47 +0700

    updated folder structure, removed unnecessary files from github

--- zArchived/send_file.py
+++
@@ -1,9 +0,0 @@
-# https://stackoverflow.com/questions/51401989/is-there-a-way-to-send-audio-files-to-a-flask-server
-
-@app.route('/uploadfile',methods=['GET','POST'])
-def uploadfile():
-    if request.method == 'PUT':
-        f = request.files['file']
-        filePath = "./somedir/"+secure_filename(f.filename)
-        f.save(filePath)
-        return "success"
